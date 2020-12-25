e16e4f966882a84f3ab45c4507b0a609994cb2e6
tyx <2046185302@qq.com>
2020-12-25 11:07:11 +0800

    推拉功能尚未成功实现

--- src/main/java/com/nowcoder/controller/FeedController.java
+++ src/main/java/com/nowcoder/controller/FeedController.java
@@ -19,7 +19,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-public class FeedController {
+public class FeedController {   //推拉功能尚未成功实现
     private static final Logger logger = LoggerFactory.getLogger(FeedController.class);
 
     @Autowired
